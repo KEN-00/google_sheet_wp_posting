2d346586bbf8923e7d7417a37085917405a49649
Ken CHIU <kenchiu21@gmail.com>
2021-06-03 22:30:27 +0800

    master read jinja2 template file for post HTML

--- main.py
+++ main.py
@@ -11,10 +11,12 @@ import os
 GOOGLE_SHEET_CONFIG_FILE_PATH_VAR_KEY = 'GOOGLE_SHEET_CONFIG_FILE_PATH'
 SERVICE_ACCOUNT_INFO_FILE_PATH_VAR_KEY = 'SERVICE_ACCOUNT_INFO_FILE_PATH'
 WP_API_CONFIG_FILE_PATH_VAR_KEY = 'WP_API_CONFIG_FILE_PATH'
+JINJA2_POST_TEMPLATE_FILE_PATH_VAR_KEY ='JINJA2_POST_TEMPLATE_FILE_PATH'
 
 DEFAULT_GOOGLE_SHEET_CONFIG_FILE_PATH = 'resource/google_sheet_config.json'
 DEFAULT_SERVICE_ACCOUNT_INFO_FILE_PATH = 'resource/service_account_info.json'
 DEFAULT_WP_API_CONFIG_FILE_PATH = 'resource/wp_api_config.json'
+DEFAULT_JINJA2_POST_TEMPLATE_FILE_PATH = 'resource/post_template.jinja2'
 
 
 def get_google_sheet_config():
@@ -35,6 +37,13 @@ def get_json_data(filename):
     with io.open(filename, "r", encoding="utf-8") as json_file:
         data = json.load(json_file)
         return data          
+
+
+def get_jinja2_post_template():
+    filePath = get_env_config(JINJA2_POST_TEMPLATE_FILE_PATH_VAR_KEY, DEFAULT_JINJA2_POST_TEMPLATE_FILE_PATH)
+    with io.open(filePath, "r") as jinja2_file:
+        return jinja2_file.read()
+    
 
 
 def get_env_config(key, default):
